843edf29455d164560c23eb1898f087f5bb9aa86
bodie wood <bodie1@macbook-air.lan>
2023-05-15 15:51:05 -0700

    add all expo skeleton

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-web';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello</Text>
       <StatusBar style="auto" />
+
+      <Button title='click me'/>
     </View>
   );
 }
